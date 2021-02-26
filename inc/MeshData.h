4eb7e55ae4780c7c6131c972665da1ade5f1a325
SamP-S <sam.ps1@outlook.com>
2021-02-26 01:39:42 +0000

    3D data

--- inc/MeshData.h
+++ inc/MeshData.h
@@ -1,5 +1,47 @@
 #pragma once
 
+/* RGBA Colours */
+#define RGBA_BLACK 0.0f, 0.0f, 0.0f, 1.0f
+#define RGBA_WHITE 1.0f, 1.0f, 1.0f, 1.0f
+
+#define RGBA_RED 1.0f, 0.0f, 0.0f, 1.0f
+#define RGBA_GREEN 0.0f, 1.0f, 0.0f, 1.0f
+#define RGBA_BLUE 0.0f, 0.0f, 1.0f, 1.0f
+
+#define RGBA_CYAN 0.0f, 1.0f, 1.0f, 1.0f
+#define RGBA_YELLOW 1.0f, 1.0f, 0.0f, 1.0f
+#define RGBA_MAGENTA 1.0f, 0.0f, 0.0f, 1.0f
+
+#define RGBA_TRANSPARENT 1.0f, 1.0f, 1.0f, 0.0f
+#define RGBA_TRANSLUCENT 1.0f, 1.0f, 1.0f, 0.5f
+
+/* ARGB Colours */
+#define ARGB_BLACK 1.0f, 0.0f, 0.0f, 0.0f
+#define ARGB_WHITE 1.0f, 1.0f, 1.0f, 1.0f
+
+#define ARGB_RED 1.0f, 1.0f, 0.0f, 0.0f
+#define ARGB_GREEN 1.0f, 0.0f, 1.0f, 0.0f
+#define ARGB_BLUE 1.0f, 0.0f, 0.0f, 1.0f
+
+#define ARGB_CYAN 1.0f, 0.0f, 1.0f, 1.0f
+#define ARGB_YELLOW 1.0f, 1.0f, 1.0f, 0.0f
+#define ARGB_MAGENTA 1.0f, 1.0f, 0.0f, 0.0f
+
+#define ARGB_TRANSPARENT 0.0f, 1.0f, 1.0f, 1.0f
+#define ARGB_TRANSLUCENT 0.5f, 1.0f, 1.0f, 1.0f
+
+/* RGB Colours*/
+#define RGB_BLACK 0.0f, 0.0f, 0.0f
+#define RGB_WHITE 1.0f, 1.0f, 1.0f
+
+#define RGB_RED 1.0f, 0.0f, 0.0f
+#define RGB_GREEN 0.0f, 1.0f, 0.0f
+#define RGB_BLUE 0.0f, 0.0f, 1.0f
+
+#define RGB_CYAN 0.0f, 1.0f, 1.0f
+#define RGB_YELLOW 1.0f, 1.0f, 0.0f
+#define RGB_MAGENTA 1.0f, 0.0f, 0.0f
+
 /* Vertex Data */
 class SimpleMesh {
 	public:
