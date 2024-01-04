4ff91940e844bf02cec2bad60ee2f6e87f726542
SamP-S <sam.ps1@outlook.com>
2024-01-04 15:38:39 +0000

    Impl main driver use application

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-#include "graphics.h"
-
+#include "application.h"
 
 int main(int argc, char *argv[])
 {
-    WindowManager window = WindowManager(1920, 1080);
-    GraphicsEngine Graphics = GraphicsEngine(&window);
-    for (int i = 0; i < 1200; i++) {
-        Graphics.Render();
-    }
+    // instance main application
+    std::cout << "start of program" << std::endl;
+    Application app = Application();// }
     std::cout << "end of program" << std::endl;
     return 0;
 }
